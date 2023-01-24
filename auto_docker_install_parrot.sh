6138e81b7a26d004907e137eea4114f603cb3b00
0xdiego <93290842+diegojoel301@users.noreply.github.com>
2023-01-24 04:31:01 +0100

    Update auto_docker_install_parrot.sh

--- auto_docker_install_parrot.sh
+++ auto_docker_install_parrot.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
-# From https://www.hiroom2.com/2017/09/24/parrotsec-3-8-docker-engine-en/
+
+# Antes para este script una vez guardado, ejecutar:
+# chmod +x <nombre_script>.sh
+# ./<nombre_script>.sh
 
 set -e
 
-# Install dependencies.
 sudo apt install -y curl apt-transport-https \
      software-properties-common ca-certificates
 
-# Install docker.
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 echo "deb [arch=amd64] https://download.docker.com/linux/debian stretch stable" | \
   sudo tee /etc/apt/sources.list.d/docker-engine.list
@@ -15,12 +16,9 @@ echo "deb [arch=amd64] https://download.docker.com/linux/debian stretch stable" 
 sudo apt-get update -y
 sudo apt-get install -y docker-ce
 
-# Run docker.
 sudo systemctl start docker
 sudo systemctl enable docker
 
-# Add user to docker group for using docker without sudo command.
 sudo gpasswd -a "${USER}" docker
 
-# Reboot
 sudo reboot
